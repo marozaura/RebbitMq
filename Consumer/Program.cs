26f1dd782447e9defe68c61eacdab6452c428377
MarozauRo <marozau.ra@gmail.com>
2024-07-13 13:53:34 +0300

    Rabbit mq exchange direct example

---
+++ Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "direct_logs", ExchangeType.Direct);
+
+var queueName = chanel.QueueDeclare().QueueName;
+
+chanel.QueueBind(queueName, "direct_logs", "error");
+
+var consumer  = new EventingBasicConsumer(chanel);
+
+consumer.Received += delegate(object? sender, BasicDeliverEventArgs eventArgs)
+{
+    var body = eventArgs.Body;
+
+    var message = Encoding.UTF8.GetString(body.ToArray());
+
+    Console.WriteLine($"{message}");
+};
+
+chanel.BasicConsume(queue: queueName,
+    autoAck:true,
+    consumer: consumer);
+
+Console.WriteLine($"Subscribe queue: {queueName}");
+Console.ReadLine();
---
+++ Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+Task.Run(PublishMessage(10000, "error"));
+//Task.Run(PublishMessage(12000, "warning"));
+
+Console.ReadLine();
+
+static Func<Task> PublishMessage(int timeToSleepTo, string routingKey)
+{
+    return () =>
+    {
+        var counter = 0;
+        do
+        {
+
+            var timeToSleep = new Random().Next(1000, timeToSleepTo);
+            Thread.Sleep(timeToSleep);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var chanel = connection.CreateModel();
+
+            chanel.ExchangeDeclare(exchange: "direct_logs", ExchangeType.Direct);
+
+            counter++;
+            var message = $"message type [error] from publisher {counter}";
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            chanel.BasicPublish(exchange: "direct_logs",
+                routingKey: routingKey,
+                basicProperties: null,
+                body: messageBytes);
+
+            Console.WriteLine($"message type {routingKey} sent to direct exchange #{counter}");
+        } while (true);
+    };
+}
+
+
+
